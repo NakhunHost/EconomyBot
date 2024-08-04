92fe608ae48292296e8b22729c327a897e7f7d82
NakhunHost <presidentialrealmsstudio@gmail.com>
2024-08-04 17:24:29 +0700

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "MTI2OTYwMTYyMjUyNzU3ODExMw.GN01Qs.9R-QXKX1RYc-6ITxx3VPeEM0GBV47KdfJoRHEQ",
+    prefix: "/",
     admins: [
         "PEOPLE WHO CAN USE ADD MONEY (IDS)"
 ],
