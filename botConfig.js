30eba11d6337987c4d89bf879e7eda5ada491bac
NakhunHost <presidentialrealmsstudio@gmail.com>
2025-02-12 16:16:30 +0700

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "MTI2OTYwMTYyMjUyNzU3ODExMw.GN01Qs.9R-QXKX1RYc-6ITxx3VPeEM0GBV47KdfJoRHEQ",
+    token: "",
     prefix: "/",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "1211510829283934209"
 ],
     debug: true,
     countChannel: "countChannelID"
